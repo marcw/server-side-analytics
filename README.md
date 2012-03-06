79156684c2fd009d61b1c41da562c33422b2491b
marc.weistroff <marc.weistroff@sensio.com>
2012-03-06 12:25:14 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Server Side Google Analytics
+============================
+
+This is a rewrite of the ga.php script given by google.
+
+[More informations here](http://code.google.com/apis/analytics/docs/mobile/mobileWebsites.html)
---
+++ src/MarcW/ServerSideAnalytics/Analytics.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace MarcW\ServerSideAnalytics;
+
+use Buzz\Browser;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Cookie;
+
+/**
+ * Analytics
+ *
+ * @author Marc Weistroff <marc@weistroff.net>
+ */
+class Analytics
+{
+    const VERSION = '4.4sh';
+
+    private $utmGifUrl = 'http://www.google-analytics.com/__utm.gif';
+
+    private $browser;
+    private $cookieName;
+    private $cookiePath;
+    private $cookiePersistence;
+
+    private $request;
+
+    public function __construct(Browser $browser, $cookieName = '__utmmobile', $cookiePath = '/', $cookiePersistence = 63072000)
+    {
+        $this->browser = $browser;
+        $this->cookieName = $cookieName;
+        $this->cookiePath = $cookiePath;
+        $this->cookiePersistence = $cookiePersistence;
+        $this->gifData =  array(
+            chr(0x47), chr(0x49), chr(0x46), chr(0x38), chr(0x39), chr(0x61),
+            chr(0x01), chr(0x00), chr(0x01), chr(0x00), chr(0x80), chr(0xff),
+            chr(0x00), chr(0xff), chr(0xff), chr(0xff), chr(0x00), chr(0x00),
+            chr(0x00), chr(0x2c), chr(0x00), chr(0x00), chr(0x00), chr(0x00),
+            chr(0x01), chr(0x00), chr(0x01), chr(0x00), chr(0x00), chr(0x02),
+            chr(0x02), chr(0x44), chr(0x01), chr(0x00), chr(0x3b)
+        );
+    }
+
+    public function track(Request $request)
+    {
+        $this->request = $request;
+
+        $referer = $this->request->query->get('utmr', '');
+        $path = $this->request->query->get('utmp', '');
+        $account = $this->request->query->get('utmac');
+        $userAgent = $this->request->server->get('HTTP_USER_AGENT', '');
+        $visitorId = $this->request->cookies->get($this->cookieName);
+        if (empty($visitorId)) {
+            $visitorId = $this->getVisitorId($this->getGuidHeader(), $account, $userAgent);
+        }
+
+        $url = $this->constructGifUrl($referer, $path, $account, $visitorId, $this->maskVisitorIp());
+        $this->browser->get($url, array(
+            'Accept-Language: '.$acceptLanguage,
+            'User-Agent: '.$userAgent;
+        ));
+
+        $cookie = new Cookie($this->cookieName, $visitorId, time() + $this->cookiePersistence, $this->cookiePath, $domain);
+        $response = new Response();
+        $response->headers->add('Content-Type', 'image/gif');
+        $response->headers->add('Pragma', 'no-cache');
+        $response->setPrivate();
+        $response->mustRevalidate();
+        $response->setExpires(new \DateTime('-10 year'));
+        $response->setContent(join($this->gifData));
+        $response->headers->setCookie($cookie);
+
+        return $response;
+    }
+
+    private function getGuidHeader()
+    {
+        $guidHeader = $this->request->server->get('HTTP_X_DCMGUID');
+        if (empty($guidHeader)) {
+            $guidHeader = $this->request->server->get('HTTP_X_UP_SUBNO');
+        }
+        if (empty($guidHeader)) {
+            $guidHeader = $this->request->server->get('HTTP_X_JPHONE_UID');
+        }
+        if (empty($guidHeader)) {
+            $guidHeader = $this->request->server->get('HTTP_X_EM_UID');
+        }
+
+        return $guidHeader;
+    }
+
+    private function getVisitorId($guid, $account, $userAgent, $cookie)
+    {
+        $message = '';
+        if (!empty($guid)) {
+            $message = $guid.$account;
+        } else {
+            $message = $userAgent.uniqid($this->getRandomNumber(), true);
+        }
+
+        $hash = md5($message);
+
+        return '0x'.substr($hash, 0, 16);
+    }
+
+    private function maskVisitorIp()
+    {
+        $ip = $this->request->getClientIp();
+        $regex = "/^([^.]+\.[^.]+\.[^.]+\.).*/";
+        if (preg_match($regex, $ip, $matches)) {
+            return $matches[1] . "0";
+        } else {
+            return "";
+        }
+    }
+
+    private function constructGifUrl($domain, $referer, $path, $account, $visitorId, $ip)
+    {
+        $query = http_build_query(array(
+            'utmwv'  => self::VERSION,
+            'utmn'   => $this->getRandomNumber(),
+            'utmhn'  => $domain,
+            'utmr'   => $referer,
+            'utmp'   => $path,
+            'utmac'  => $account,
+            'utmcc'  => '__utma=999.999.999.999.999.1;',
+            'utmvid' => $visitorId,
+            'utmip'  => $ip,
+        ));
+
+        return $this->utmGifUrl.'?'.$query;
+    }
+
+    private function getRandomNumber()
+    {
+        return rand(0, 0x7fffffff);
+    }
+}
+
